9c87fe178979f88d317563f0d8283cc7ae09bcf1
Anton Ignacious Andrew <andrewkavin30@gmail.com>
2026-01-07 09:59:41 +0530

    Fix navbar alignment by removing hardcoded margins and using flexbox

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,15 +5,15 @@ import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <nav className='navbar'> 
-        <div className='navbar-brand'>
-          <img src={logo} className='logo' />
-        </div>
+    <nav className='navbar'>
+      <div className='navbar-brand'>
+        <img src={logo} className='logo' />
         <Link to='/' className='title-infi'>Infinity Movies</Link>
-        <div className='navbar-links'>
-            <Link to='/' className='nav-link'>Home</Link>
-            <Link to='/fav' className='nav-link'>Favorites</Link>
-        </div>
+      </div>
+      <div className='navbar-links'>
+        <Link to='/' className='nav-link'>Home</Link>
+        <Link to='/fav' className='nav-link'>Favorites</Link>
+      </div>
     </nav>
   )
 }
